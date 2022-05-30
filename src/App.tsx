6e01be7ea9ee64fc7c43e2eeb6bca3bd7da28f55
mitya-me <dmitrymefodyev@gmail.com>
2022-05-30 23:10:04 +0300

    Add check auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import {FC} from 'react' 
+import {FC, useEffect} from 'react' 
 import AppRouter from './components/AppRouter';
 import './App.css'
 import NavBar from './components/NavBar';
 import { Layout } from 'antd';
 import './App.css'
+import { useActions } from './hooks/useActions';
+import { IUser } from './models/IUser';
 
 const App:FC = () => { 
-  return (
-      <Layout>
-          <NavBar />
-          <Layout.Content>
-              <AppRouter />
-          </Layout.Content>
-      </Layout>
-  )
+    const { setAuth, setUser} = useActions() 
+
+    useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setUser({username: localStorage.getItem('username' || '')} as IUser)
+            setAuth(true)
+        }
+    })
+
+    return (
+        <Layout>
+            <NavBar />
+                <Layout.Content>
+                    <AppRouter />
+                </Layout.Content>
+        </Layout>
+    )
 }
 
 export default App;
