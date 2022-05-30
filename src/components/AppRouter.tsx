31b54064b660311befcec4cd512ffc6a55d92b79
mitya-me <dmitrymefodyev@gmail.com>
2022-05-30 23:28:18 +0300

    smallRefact: remove excess import

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react' 
-import { useSelector } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import { privateRoutes, publicRoutes, RouteNames } from '../router';
