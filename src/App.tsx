1a6336c11706b9a8de5400b193aa21f7980dcfe8
mitya-me <dmitrymefodyev@gmail.com>
2022-05-30 17:37:27 +0300

    Add antd layout for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import {FC} from 'react' 
+import AppRouter from './components/AppRouter';
+import './App.css'
+import NavBar from './components/NavBar';
+import { Layout } from 'antd';
+import './App.css'
 
 const App:FC = () => { 
-
   return (
-    <div>
-      welcome! basic template ready to use!
-    </div>
+      <Layout>
+          <NavBar />
+          <Layout.Content>
+              <AppRouter />
+          </Layout.Content>
+      </Layout>
   )
 }
 
