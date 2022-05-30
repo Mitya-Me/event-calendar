7f7f2c61d3a550cf451d9f7f194e0f97538a9d42
mitya-me <dmitrymefodyev@gmail.com>
2022-05-30 23:27:12 +0300

    Refact: useActions for logout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,23 @@
 import { Layout, Menu, Row } from 'antd';
 import {FC} from 'react' 
 import { useHistory } from 'react-router-dom';
+import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import { RouteNames } from '../router';
 
 const NavBar:FC = () => { 
-    const router = useHistory();
-    const { isAuth } = useTypedSelector(state => state.auth)
+    const router = useHistory()
+    const { isAuth, user } = useTypedSelector(state => state.auth)
+    const { logout } = useActions()
 
     return (
         <Layout.Header>
             <Row justify="end">
                 {isAuth 
                 ?   <>
-                        <div style={{ color: "white" }}> User 1 </div>
+                        <div style={{ color: "white" }}> {user.username} </div>
                         <Menu theme="dark" mode="horizontal" selectable={false}>
-                            <Menu.Item key={1} onClick={() => console.log("EXIT")}>
+                            <Menu.Item key={1} onClick={() => logout()}>
                                 Exit
                             </Menu.Item>
                         </Menu>
