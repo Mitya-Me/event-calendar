cd859ed91df2ea778699fd868d14551152d76f15
mitya-me <dmitrymefodyev@gmail.com>
2022-05-30 23:14:46 +0300

    Add: enum AuthAction and interfaces for actions

--- src/store/reducers/auth/types.ts
+++ src/store/reducers/auth/types.ts
@@ -1,14 +1,36 @@
+import { IUser } from './../../../models/IUser';
 export default interface IAuthState {
     isAuth: boolean;
+    user: IUser;
+    isLoading: boolean;
+    error: string;
 }
 
 export enum AuthActionEnum {
-    SET_AUTH = 'SET_AUTH'
+    SET_AUTH = 'SET_AUTH',
+    SET_ERROR = 'SET_ERROR',
+    SET_USER = 'SET_USER',
+    SET_IS_LOADING = 'SET_IS_LOADING'
 }
 
-interface ISetAuthAction {
+export interface ISetAuthAction {
     type: AuthActionEnum.SET_AUTH;
     payload: boolean;
 }
 
-export type AuthAction = ISetAuthAction 
+export interface ISetErrorAction {
+    type: AuthActionEnum.SET_ERROR;
+    payload: string;
+}
+
+export interface ISetUserAction {
+    type: AuthActionEnum.SET_USER;
+    payload: IUser;
+}
+
+export interface ISetIsLoadingAction {
+    type: AuthActionEnum.SET_IS_LOADING;
+    payload: boolean;
+}
+
+export type AuthAction = ISetAuthAction | ISetErrorAction | ISetUserAction | ISetIsLoadingAction;
